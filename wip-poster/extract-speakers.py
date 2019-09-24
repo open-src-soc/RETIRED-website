2a58504e96bf16916e2f8f984220c04af05d44ad
Thomas Hiscock <thomas.hiscock@wanadoo.fr>
2019-09-24 07:18:24 +0200

    poster: fix for recent pandoc version
    Rencent pandoc emit annoying hypertargets, we need to handle them when
    removing sections.

--- wip-poster/extract-speakers.py
+++ wip-poster/extract-speakers.py
@@ -4,9 +4,17 @@
 import collections
 import pathlib
 import re
+import subprocess
+
 
 SECTION_RE = re.compile(r"\\section{([^}]+)}(.*)$")
 SUBSECTION_RE = re.compile(r"\\subsection{([^}]+)}(.*)$")
+
+
+def pandoc_version():
+    stdout = subprocess.check_output(['pandoc', '--version'])
+    version_str = stdout.splitlines()[0].split()[1].decode('utf-8')
+    return tuple(map(int, version_str.split('.')))
 
 
 def main():
@@ -15,14 +23,29 @@ def main():
         print("USAGE {} INPUT.tex", sys.argv[0])
         sys.exit(1)
 
+    strip_hypertarget = False
+    try:
+        major, _, _ = pandoc_version()
+        if major >= 2:
+            # FIXME(ThomasH, 24 Sep 2019): not sure of the exact version
+            strip_hypertarget = True
+    except FileNotFoundError:
+        print("Unable to find a valid pandoc executable, please make sure that "
+              "**pandoc is installed**!", file=sys.stderr)
+        sys.exit(1)
+    except Exception as e:
+        print("WARNING: unable to query pandoc version,",
+              "got exception '{}' ({})".format(e, e.__class__),
+              file=sys.stderr)
+
     for l in pathlib.Path(sys.argv[1]).open():
         if l.startswith("\\emph") or l.startswith("/"):
             # Speaker bio...
             continue
         res = SECTION_RE.match(l)
         if res:
-            print("% Section {} | {}".format(res.group(0), res.group(1)))
-            print("\n")
+            if strip_hypertarget:
+                print("}")
             continue
 
         res = SUBSECTION_RE.match(l)
